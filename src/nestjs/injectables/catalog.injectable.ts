9f1c0fe6121bcda45b1b88de41a4bf54960e6d94
Przemyslaw Walczak <p.d.walczak@gmail.com>
2023-10-07 21:24:19 +0200

    chore: refactoring

--- src/nestjs/injectables/catalog.injectable.ts
+++ src/nestjs/injectables/catalog.injectable.ts
@@ -1,20 +1,15 @@
 import { Injectable } from '@nestjs/common'
 import { ClassConstructor, Document, ElasticsearchCatalog } from 'lib/types'
 import { QueryBuilder } from 'lib/builders'
-import { ELASTICSEARCH_CATALOG_NAME } from 'lib/constants'
 import { SearchOptions } from 'lib/elasticsearch'
 import { ElasticsearchService } from '..'
 
 @Injectable()
 export class Catalog<TDocument extends Document> implements ElasticsearchCatalog<TDocument> {
-    private readonly index: string
-
     constructor(
         private readonly service: ElasticsearchService,
         private readonly document: ClassConstructor<TDocument>
-    ) {
-        this.index = Reflect.getMetadata(ELASTICSEARCH_CATALOG_NAME, document)
-    }
+    ) {}
 
     search(options: SearchOptions<TDocument>) {
         return this.service.search(this.document, options)
