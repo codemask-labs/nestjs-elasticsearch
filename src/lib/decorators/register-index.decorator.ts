1d71200541ee96e42ec026b56a9c6ad2898df7fe
Przemyslaw Walczak <p.d.walczak@gmail.com>
2023-10-09 11:26:27 +0200

    chore: formatting

--- src/lib/decorators/register-index.decorator.ts
+++ src/lib/decorators/register-index.decorator.ts
@@ -1,9 +1,7 @@
 import { ELASTICSEARCH_INDEX_NAME_METADATA } from 'lib/constants'
 
-export const RegisterIndex =
-    (name: string) =>
-    <T>(constructor: new () => T): new () => T => {
-        Reflect.defineMetadata(ELASTICSEARCH_INDEX_NAME_METADATA, name, constructor)
+export const RegisterIndex =(name: string) => <T>(constructor: new () => T): new () => T => {
+    Reflect.defineMetadata(ELASTICSEARCH_INDEX_NAME_METADATA, name, constructor)
 
-        return constructor
-    }
+    return constructor
+}
