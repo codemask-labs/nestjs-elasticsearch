60111819ca5a2ab3790a9cfe8e697fe0bf7fd1a2
Przemyslaw Walczak (Windows) <p.d.walczak@gmail.com>
2023-10-21 17:59:27 +0200

    test: getting elasticsearch cluster health

--- src/lib/enums/health-status.enum.ts
+++ src/lib/enums/health-status.enum.ts
@@ -1,5 +1,14 @@
 export enum HealthStatus {
+    /**
+     * @description All shards are assigned.
+     */
     Green = 'green',
-    Orange = 'orange',
+    /**
+     * @description All primary shards are assigned, but one or more replica shards are unassigned. If a node in the cluster fails, some data could be unavailable until that node is repaired.
+     */
+    Yellow = 'yellow',
+    /**
+     * @description One or more primary shards are unassigned, so some data is unavailable. This can occur briefly during cluster startup as primary shards are assigned.
+     */
     Red = 'red'
 }
---
+++ src/test/features/3-checking-cluster-health.spec.ts
@@ -0,0 +1,28 @@
+import { setupNestApplication } from 'test/toolkit'
+import { ElasticsearchModule } from 'nestjs/elasticsearch.module'
+import { ElasticsearchService } from 'nestjs/elasticsearch.service'
+import { HealthStatus } from 'lib/enums'
+
+describe('Making a search', () => {
+    const { app } = setupNestApplication({
+        imports: [
+            ElasticsearchModule.register({
+                node: 'http://localhost:9200'
+            })
+        ]
+    })
+
+    it('has service available', () => {
+        const service = app.get(ElasticsearchService)
+
+        expect(service).toBeDefined()
+    })
+
+    it('health of a cluster being green', async () => {
+        const service = app.get(ElasticsearchService)
+        const result = await service.getClusterHealth()
+        const statues = Object.values(HealthStatus)
+
+        expect(statues.includes(result.status)).toBeTruthy()
+    })
+})
