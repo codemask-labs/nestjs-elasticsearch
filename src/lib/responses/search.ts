150eecd9299c5c5c8df3ea1a7449ae9cffaf3274
Przemyslaw Walczak (Windows) <p.d.walczak@gmail.com>
2023-10-21 22:33:05 +0200

    chore: removed dangling comments

--- src/lib/responses/search.ts
+++ src/lib/responses/search.ts
@@ -3,19 +3,14 @@ import { ClassConstructor, Document, Result } from 'lib/common'
 import { Aggregations } from 'lib/aggregations'
 import { TransformedAggregations } from 'lib/transformers'
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export type SearchResponse<TDocument extends Document, TAggregationsBody extends Record<string, Aggregations<TDocument>>> = {
     documents: Array<TDocument>
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     aggregations: TransformedAggregations<TDocument, TAggregationsBody>
 }
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const getSearchResponse = <TDocument extends Document, TAggregationsBody extends Record<string, Aggregations<TDocument>>>(
     document: ClassConstructor<TDocument>,
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     { body }: ApiResponse<Result<TDocument, TAggregationsBody>>
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
 ): SearchResponse<TDocument, TAggregationsBody> => ({
     documents: body.hits.hits.reduce((result, { _source: source }) => {
         if (!source) {
@@ -24,6 +19,5 @@ export const getSearchResponse = <TDocument extends Document, TAggregationsBody 
 
         return [...result, Object.assign(new document(), source)]
     }, [] as Array<TDocument>),
-    // eslint-disable-next-line @typescript-eslint/ban-types
     aggregations: (body.aggregations || {}) as TransformedAggregations<TDocument, TAggregationsBody>
 })
