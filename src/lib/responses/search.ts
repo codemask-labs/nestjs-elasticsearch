3da140814683eed03f0688595b5e953a8329534c
Przemyslaw Walczak (Windows) <p.d.walczak@gmail.com>
2023-10-21 22:20:55 +0200

    feat: extracting aggregation types with nested aggregations

--- src/lib/responses/search.ts
+++ src/lib/responses/search.ts
@@ -1,18 +1,21 @@
 import type { ApiResponse } from '@elastic/elasticsearch'
 import { ClassConstructor, Document, Result } from 'lib/common'
 import { Aggregations } from 'lib/aggregations'
+import { TransformedAggregations } from 'lib/transformers'
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export type SearchResponse<TDocument extends Document, TAggregationsBody extends Record<string, Aggregations<TDocument>>> = {
     documents: Array<TDocument>
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    aggregations: Record<string, any>
+    aggregations: TransformedAggregations<TDocument, TAggregationsBody>
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const getSearchResponse = <TDocument extends Document, TAggregationsBody extends Record<string, Aggregations<TDocument>>>(
     document: ClassConstructor<TDocument>,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     { body }: ApiResponse<Result<TDocument, TAggregationsBody>>
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 ): SearchResponse<TDocument, TAggregationsBody> => ({
     documents: body.hits.hits.reduce((result, { _source: source }) => {
         if (!source) {
@@ -21,5 +24,6 @@ export const getSearchResponse = <TDocument extends Document, TAggregationsBody 
 
         return [...result, Object.assign(new document(), source)]
     }, [] as Array<TDocument>),
-    aggregations: body.aggregations || {}
+    // eslint-disable-next-line @typescript-eslint/ban-types
+    aggregations: (body.aggregations || {}) as TransformedAggregations<TDocument, TAggregationsBody>
 })
---
+++ src/test/features/4-making-aggregation.spec.ts
@@ -0,0 +1,76 @@
+import 'expect-more-jest'
+import { setupNestApplication } from 'test/toolkit'
+import { HomeDocument, TestService } from 'test/module'
+import { ElasticsearchModule } from 'module/elasticsearch.module'
+
+describe('Making a search', () => {
+    const { app } = setupNestApplication({
+        providers: [TestService],
+        imports: [
+            ElasticsearchModule.register({
+                node: 'http://localhost:9200'
+            }),
+            ElasticsearchModule.forFeature([HomeDocument])
+        ]
+    })
+
+    it('has index available', () => {
+        expect(app.get(TestService)).toBeDefined()
+    })
+
+    it('makes a aggregation with nested average area squared or null aggregation', async () => {
+        const service = app.get(TestService)
+        const result = await service.getTopAddressesWithHighestAverageAreaSquared()
+
+        result.aggregations.b.buckets.map(bucket => bucket.averageSquareMeters.value)
+
+        expect(result.documents).toEqual([])
+        expect(result.aggregations).toEqual({
+            a: {
+                // eslint-disable-next-line camelcase
+                doc_count_error_upper_bound: expect.any(Number),
+                // eslint-disable-next-line camelcase
+                sum_other_doc_count: expect.any(Number),
+                buckets: expect.arrayContaining([
+                    {
+                        // eslint-disable-next-line camelcase
+                        doc_count: expect.any(Number),
+                        key: expect.any(String)
+                    }
+                ])
+            },
+            b: {
+                // eslint-disable-next-line camelcase
+                doc_count_error_upper_bound: expect.any(Number),
+                // eslint-disable-next-line camelcase
+                sum_other_doc_count: expect.any(Number),
+                buckets: expect.arrayContaining([
+                    {
+                        // eslint-disable-next-line camelcase
+                        doc_count: expect.any(Number),
+                        key: expect.any(String),
+                        averageSquareMeters: {
+                            value: expect.toBeNullableOf(expect.any(Number))
+                        }
+                    }
+                ])
+            },
+            homes: {
+                // eslint-disable-next-line camelcase
+                doc_count_error_upper_bound: expect.any(Number),
+                // eslint-disable-next-line camelcase
+                sum_other_doc_count: expect.any(Number),
+                buckets: expect.arrayContaining([
+                    {
+                        // eslint-disable-next-line camelcase
+                        doc_count: expect.any(Number),
+                        key: expect.any(String),
+                        averageSquareMeters: {
+                            value: expect.toBeNullableOf(expect.any(Number))
+                        }
+                    }
+                ])
+            }
+        })
+    })
+})
