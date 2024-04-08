9e1016b18d1a76a84f08bbb6a0afe249cd0335f6
przemyslawwalczak <p.d.walczak@gmail.com>
2024-04-08 13:42:57 +0200

    fix: get top hits aggregation (#78)

--- src/lib/aggregations/get-top-hits.spec.ts
+++ src/lib/aggregations/get-top-hits.spec.ts
@@ -1,16 +1,25 @@
+import { Order } from 'lib/enums'
 import { HomeDocument } from 'test/module'
 import { getTopHitsAggregation } from './get-top-hits'
 
 describe('getTopHitsAggregation', () => {
     it('accepts only schema field', () => {
         const query = getTopHitsAggregation<HomeDocument>(1, {
-            includes: ['address', 'city', 'fullName']
+            from: 0,
+            includes: ['address', 'city', 'fullName'],
+            sort: [
+                { address: { order: Order.ASC } }
+            ]
         })
 
         expect(query).toEqual({
             // eslint-disable-next-line camelcase
             top_hits: {
                 size: 1,
+                from: 0,
+                sort: [
+                    { address: { order: Order.ASC } }
+                ],
                 _source: {
                     includes: ['address', 'city', 'fullName']
                 }
