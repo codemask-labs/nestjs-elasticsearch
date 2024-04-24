cfbf46051c244ba0509cf3b8baa890f626156594
przemyslawwalczak <p.d.walczak@gmail.com>
2024-04-24 15:22:29 +0200

    fix: stats bucket aggregation (#105)

--- src/lib/decorators/register-index.decorator.ts
+++ src/lib/decorators/register-index.decorator.ts
@@ -2,8 +2,8 @@ import { ELASTICSEARCH_INDEX_NAME_METADATA } from 'lib/constants'
 
 export const RegisterIndex =
     (name: string) =>
-        <T>(constructor: new () => T): new () => T => {
-            Reflect.defineMetadata(ELASTICSEARCH_INDEX_NAME_METADATA, name, constructor)
+    <T>(constructor: new () => T): new () => T => {
+        Reflect.defineMetadata(ELASTICSEARCH_INDEX_NAME_METADATA, name, constructor)
 
-            return constructor
-        }
+        return constructor
+    }
