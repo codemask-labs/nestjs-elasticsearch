6d710b49c07c9c9797603d9fb608ef5445681b16
ada-krupa <119587056+ada-krupa@users.noreply.github.com>
2024-05-29 15:39:31 +0200

    fix: update get avg to supports only numeric fields (#130)
    According to
    [documentation](https://www.elastic.co/guide/en/elasticsearch/reference/7.17/search-aggregations-metrics-avg-aggregation.html#search-aggregations-metrics-avg-aggregation)
    avg aggregation supports only numeric fields.

--- src/lib/aggregations/get-avg.ts
+++ src/lib/aggregations/get-avg.ts
@@ -1,15 +1,13 @@
-import { Document, Field } from 'lib/common'
+import { Document, NumericField } from 'lib/common'
 
 export type AvgAggregationBody<TDocument extends Document> = {
-    field: Field<TDocument>
+    field: NumericField<TDocument>
 }
 
 export type AvgAggregation<TDocument extends Document> = {
     avg: AvgAggregationBody<TDocument>
 }
 
-export const getAvgAggregation = <TDocument extends Document, TField extends Field<TDocument> = Field<TDocument>>(
-    field: TField
-): AvgAggregation<TDocument> => ({
+export const getAvgAggregation = <TDocument extends Document>(field: NumericField<TDocument>): AvgAggregation<TDocument> => ({
     avg: { field }
 })
