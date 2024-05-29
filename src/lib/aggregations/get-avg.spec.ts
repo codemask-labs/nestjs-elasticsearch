1f9dc35e65198b7d8f6c076f48c68894c2285c8e
Ada Krupa <adriannakrupa@codemask.com>
2024-05-29 15:04:14 +0200

    fix: update get avg to supports only numeric fields

--- src/lib/aggregations/get-avg.spec.ts
+++ src/lib/aggregations/get-avg.spec.ts
@@ -1,16 +1,78 @@
+import { ResponseError } from '@elastic/elasticsearch/lib/errors.js'
 import { HomeDocument } from 'test/module'
+import { TEST_ELASTICSEARCH_NODE } from 'test/constants'
+import { setupNestApplication } from 'test/toolkit'
+import { ElasticsearchModule } from 'module/elasticsearch.module'
+import { ElasticsearchService } from 'module/elasticsearch.service'
 import { getAvgAggregation } from './get-avg'
 
 describe('getAvgAggregation', () => {
+    const { app } = setupNestApplication({
+        imports: [
+            ElasticsearchModule.register({
+                node: TEST_ELASTICSEARCH_NODE
+            })
+        ]
+    })
+
     it('accepts only schema field', () => {
-        const query = getAvgAggregation<HomeDocument>('address')
+        const query = getAvgAggregation<HomeDocument>('builtInYear')
 
         expect(query).toEqual({
             avg: {
-                field: 'address'
+                field: 'builtInYear'
             }
         })
     })
 
-    test.todo('accepts only schema field with keyword')
+    it('should queries elasticsearch for avg aggregation ', async () => {
+        const service = app.get(ElasticsearchService)
+
+        const result = await service.search(HomeDocument, {
+            size: 0,
+            aggregations: {
+                testAggregation: getAvgAggregation('builtInYear')
+            }
+        })
+
+        expect(result.aggregations.testAggregation.value).toBeDefined()
+    })
+
+    it(`should return an error when after passing 'keyword'`, async () => {
+        const service = app.get(ElasticsearchService)
+
+        await service
+            .search(HomeDocument, {
+                size: 0,
+                aggregations: {
+                    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                    testAggregation: getAvgAggregation('address.keyword') as any
+                }
+            })
+            .catch(error => {
+                expect(error).toBeInstanceOf(ResponseError)
+                expect(error.message).toContain('search_phase_execution_exception: [illegal_argument_exception]')
+                expect(error.message).toContain('Field [address.keyword] of type [keyword] is not supported for aggregation [avg]')
+            })
+    })
+
+    it(`should return an error when after passing 'keyword'`, async () => {
+        const service = app.get(ElasticsearchService)
+
+        await service
+            .search(HomeDocument, {
+                size: 0,
+                aggregations: {
+                    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                    testAggregation: getAvgAggregation('address') as any
+                }
+            })
+            .catch(error => {
+                expect(error).toBeInstanceOf(ResponseError)
+                expect(error.message).toContain('search_phase_execution_exception: [illegal_argument_exception]')
+                expect(error.message).toContain(
+                    'Text fields are not optimised for operations that require per-document field data like aggregations and sorting, so these operations are disabled by default.'
+                )
+            })
+    })
 })
--- src/lib/aggregations/get-avg.ts
+++ src/lib/aggregations/get-avg.ts
@@ -1,15 +1,17 @@
-import { Document, Field } from 'lib/common'
+import { Document } from 'lib/common'
+
+type NumericField<TDocument extends Document> = {
+    [K in keyof TDocument]: Exclude<TDocument[K], undefined> extends number ? K : never
+}[keyof TDocument]
 
 export type AvgAggregationBody<TDocument extends Document> = {
-    field: Field<TDocument>
+    field: NumericField<TDocument>
 }
 
 export type AvgAggregation<TDocument extends Document> = {
     avg: AvgAggregationBody<TDocument>
 }
 
-export const getAvgAggregation = <TDocument extends Document, TField extends Field<TDocument> = Field<TDocument>>(
-    field: TField
-): AvgAggregation<TDocument> => ({
+export const getAvgAggregation = <TDocument extends Document>(field: NumericField<TDocument>): AvgAggregation<TDocument> => ({
     avg: { field }
 })
