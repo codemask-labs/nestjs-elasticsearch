15f8947791514a367b961595a04813a670e2421d
Ada Krupa <adriannakrupa@codemask.com>
2024-05-29 15:11:49 +0200

    chore: improvements

--- src/lib/aggregations/get-avg.spec.ts
+++ src/lib/aggregations/get-avg.spec.ts
@@ -38,25 +38,7 @@ describe('getAvgAggregation', () => {
         expect(result.aggregations.testAggregation.value).toBeDefined()
     })
 
-    it(`should return an error when after passing 'keyword'`, async () => {
-        const service = app.get(ElasticsearchService)
-
-        await service
-            .search(HomeDocument, {
-                size: 0,
-                aggregations: {
-                    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-                    testAggregation: getAvgAggregation('address.keyword') as any
-                }
-            })
-            .catch(error => {
-                expect(error).toBeInstanceOf(ResponseError)
-                expect(error.message).toContain('search_phase_execution_exception: [illegal_argument_exception]')
-                expect(error.message).toContain('Field [address.keyword] of type [keyword] is not supported for aggregation [avg]')
-            })
-    })
-
-    it(`should return an error when after passing 'keyword'`, async () => {
+    it(`should return an error when after passing string field`, async () => {
         const service = app.get(ElasticsearchService)
 
         await service
@@ -73,6 +55,24 @@ describe('getAvgAggregation', () => {
                 expect(error.message).toContain(
                     'Text fields are not optimised for operations that require per-document field data like aggregations and sorting, so these operations are disabled by default.'
                 )
+            })
+    })
+
+    it(`should return an error when after passing string field with "keyword"`, async () => {
+        const service = app.get(ElasticsearchService)
+
+        await service
+            .search(HomeDocument, {
+                size: 0,
+                aggregations: {
+                    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                    testAggregation: getAvgAggregation('address.keyword') as any
+                }
+            })
+            .catch(error => {
+                expect(error).toBeInstanceOf(ResponseError)
+                expect(error.message).toContain('search_phase_execution_exception: [illegal_argument_exception]')
+                expect(error.message).toContain('Field [address.keyword] of type [keyword] is not supported for aggregation [avg]')
             })
     })
 })
