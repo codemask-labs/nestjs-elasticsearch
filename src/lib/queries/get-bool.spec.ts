1a94d883274bfc8fb851b2d70a65fb919f128e9c
ada-krupa <119587056+ada-krupa@users.noreply.github.com>
2024-05-29 13:17:05 +0200

    test: add unit test for get bool query with es service (#129)

--- src/lib/queries/get-bool.spec.ts
+++ src/lib/queries/get-bool.spec.ts
@@ -1,14 +1,28 @@
 import { faker } from '@faker-js/faker'
 import { getMinimumShouldMatchParameter } from 'lib/parameters'
 import { HomeDocument, PropertyType } from 'test/module'
+import { TEST_ELASTICSEARCH_NODE } from 'test/constants'
+import { setupNestApplication } from 'test/toolkit'
+import { ElasticsearchModule } from 'module/elasticsearch.module'
+import { ElasticsearchService } from 'module/elasticsearch.service'
 import { getBoolQuery } from './get-bool'
 import { getRangeQuery } from './get-range'
 import { getShouldQuery } from './get-should'
 import { getTermQuery } from './get-term'
 import { getTermsQuery } from './get-terms'
 import { getExistsQuery } from './get-exists'
+import { getMustQuery } from './get-must'
+import { getMustNotQuery } from './get-must-not'
 
 describe('getBoolQuery', () => {
+    const { app } = setupNestApplication({
+        imports: [
+            ElasticsearchModule.register({
+                node: TEST_ELASTICSEARCH_NODE
+            })
+        ]
+    })
+
     it('accepts optional must query', () => {
         const id = faker.string.uuid()
         const query = getBoolQuery<HomeDocument>({
@@ -57,6 +71,68 @@ describe('getBoolQuery', () => {
                     }
                 ]
             }
+        })
+    })
+
+    it('supports must query', async () => {
+        const service = app.get(ElasticsearchService)
+
+        const fullName = 'Stephanie Becker'
+        const result = await service.search(HomeDocument, {
+            size: 10,
+            query: getBoolQuery(getMustQuery(getTermQuery('fullName.keyword', fullName)))
+        })
+
+        result.documents.forEach(document => expect(document.fullName).toBe(fullName))
+    })
+
+    it('supports must not query', async () => {
+        const service = app.get(ElasticsearchService)
+
+        const city = 'Port Marcia'
+        const result = await service.search(HomeDocument, {
+            size: 10,
+            query: getBoolQuery(getMustNotQuery(getTermQuery('city.keyword', city)))
+        })
+
+        result.documents.forEach(document => expect(document.fullName).not.toBe(city))
+    })
+
+    it('supports should query', async () => {
+        const service = app.get(ElasticsearchService)
+
+        const fullName = 'Stephanie Becker'
+        const city = 'Johnson City'
+        const result = await service.search(HomeDocument, {
+            size: 10,
+            query: getBoolQuery(getShouldQuery([getTermQuery('city.keyword', city), getTermQuery('fullName.keyword', fullName)]))
+        })
+
+        result.documents.forEach(document => {
+            const isMatch = document.fullName === fullName || document.city === city
+
+            expect(isMatch).toBe(true)
+        })
+    })
+
+    it('supports minimum should match parameter query', async () => {
+        const service = app.get(ElasticsearchService)
+
+        const fullName = 'Irma Kilback'
+        const city = 'Johnson City'
+
+        const result = await service.search(HomeDocument, {
+            size: 10,
+            query: getBoolQuery({
+                ...getMinimumShouldMatchParameter(2),
+                ...getShouldQuery([getTermQuery('city.keyword', city), getTermQuery('fullName.keyword', fullName)])
+            })
+        })
+
+        result.documents.forEach(document => {
+            const isMatch = document.fullName === fullName && document.city === city
+
+            expect(isMatch).toBe(true)
         })
     })
 })
