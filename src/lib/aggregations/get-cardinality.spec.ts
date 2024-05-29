c30aaa504bd7306ecc4656846b8d107b8245b7a4
ada-krupa <119587056+ada-krupa@users.noreply.github.com>
2024-05-29 16:24:08 +0200

    test: add unit tests for get cardinality agg with es service (#131)

--- src/lib/aggregations/get-cardinality.spec.ts
+++ src/lib/aggregations/get-cardinality.spec.ts
@@ -1,8 +1,20 @@
 /* eslint-disable camelcase */
 import { HomeDocument } from 'test/module'
+import { TEST_ELASTICSEARCH_NODE } from 'test/constants'
+import { setupNestApplication } from 'test/toolkit'
+import { ElasticsearchModule } from 'module/elasticsearch.module'
+import { ElasticsearchService } from 'module/elasticsearch.service'
 import { getCardinalityAggregation } from './get-cardinality'
 
 describe('getCardinalityAggregation', () => {
+    const { app } = setupNestApplication({
+        imports: [
+            ElasticsearchModule.register({
+                node: TEST_ELASTICSEARCH_NODE
+            })
+        ]
+    })
+
     it('accepts only schema field', () => {
         const query = getCardinalityAggregation<HomeDocument>('address', {
             precision_threshold: 1000
@@ -16,5 +28,66 @@ describe('getCardinalityAggregation', () => {
         })
     })
 
-    test.todo('accepts only schema field with keyword')
+    it('should queries elasticsearch for cardinality aggregation with specific field', async () => {
+        const service = app.get(ElasticsearchService)
+
+        const result = await service.search(HomeDocument, {
+            size: 0,
+            aggregations: {
+                testAggregation: getCardinalityAggregation('address.keyword')
+            }
+        })
+
+        expect(result.aggregations.testAggregation.value).toBeDefined()
+    })
+
+    it('should queries elasticsearch for cardinality aggregation with specific field and precision threshold', async () => {
+        const service = app.get(ElasticsearchService)
+
+        const result = await service.search(HomeDocument, {
+            size: 0,
+            aggregations: {
+                testAggregation: getCardinalityAggregation('address.keyword', {
+                    precision_threshold: 1
+                })
+            }
+        })
+
+        expect(result.aggregations.testAggregation.value).toBeDefined()
+    })
+
+    it('should queries elasticsearch for cardinality aggregation with script', async () => {
+        const service = app.get(ElasticsearchService)
+
+        const result = await service.search(HomeDocument, {
+            size: 0,
+            aggregations: {
+                testAggregation: getCardinalityAggregation({
+                    script: `doc['address.keyword'].value`
+                })
+            }
+        })
+
+        expect(result.aggregations.testAggregation.value).toBeDefined()
+    })
+
+    it('should queries elasticsearch for cardinality aggregation with script and precision threshold', async () => {
+        const service = app.get(ElasticsearchService)
+
+        const result = await service.search(HomeDocument, {
+            size: 0,
+            aggregations: {
+                testAggregation: getCardinalityAggregation(
+                    {
+                        script: `doc['address.keyword'].value`
+                    },
+                    {
+                        precision_threshold: 1
+                    }
+                )
+            }
+        })
+
+        expect(result.aggregations.testAggregation.value).toBeDefined()
+    })
 })
