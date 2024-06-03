a6e21d878da77e6a27cc6f7caa6cc9390f798f0c
Ada Krupa <adriannakrupa@codemask.com>
2024-06-03 11:54:38 +0200

    fix: update histogram aggregation

--- src/lib/aggregations/get-histogram.ts
+++ src/lib/aggregations/get-histogram.ts
@@ -1,14 +1,23 @@
-import { Document, Key } from 'lib/common'
+import { Document, NumericField } from 'lib/common'
 
 export type HistogramAggregationBody<TDocument extends Document> = {
-    field: Key<TDocument>
+    field: NumericField<TDocument>
     interval: number
+    min_doc_count?: number
 }
 
 export type HistogramAggregation<TDocument extends Document> = {
     histogram: HistogramAggregationBody<TDocument>
 }
 
-export const getHistogramAggregation = <TDocument extends Document>(field: Key<TDocument>, interval: number): HistogramAggregation<TDocument> => ({
-    histogram: { field, interval }
+export const getHistogramAggregation = <TDocument extends Document>(
+    field: NumericField<TDocument>,
+    interval: number,
+    min_doc_count?: number
+): HistogramAggregation<TDocument> => ({
+    histogram: {
+        field,
+        interval,
+        min_doc_count
+    }
 })
