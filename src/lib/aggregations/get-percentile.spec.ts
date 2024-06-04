b2d8c8b49f1f12e452aa3c1cf873e7a5ba23068a
Ada Krupa <adriannakrupa@codemask.com>
2024-06-04 10:33:47 +0200

    test: add unit tests for get percentile aggregation

--- src/lib/aggregations/get-percentile.spec.ts
+++ src/lib/aggregations/get-percentile.spec.ts
@@ -1,3 +1,4 @@
+import { ResponseError } from '@elastic/elasticsearch/lib/errors.js'
 import { HomeDocument } from 'test/module'
 import { TEST_ELASTICSEARCH_NODE } from 'test/constants'
 import { setupNestApplication } from 'test/toolkit'
@@ -14,27 +15,27 @@ describe('getPercentileAggregation', () => {
         ]
     })
 
-    it('accepts only schema field and passed percentiles', () => {
-        const query = getPercentileAggregation<HomeDocument>('address', [5, 10, 15])
+    it('accepts only schema numeric field and passed percentiles', () => {
+        const query = getPercentileAggregation<HomeDocument>('propertyAreaSquared', [5, 10, 15])
 
         expect(query).toEqual({
             percentiles: {
-                field: 'address',
+                field: 'propertyAreaSquared',
                 percents: [5, 10, 15]
             }
         })
     })
 
-    it('queries es for percentile aggregation', async () => {
+    it('should queries elasticsearch for percentile aggregation', async () => {
         const service = app.get(ElasticsearchService)
         const result = await service.search(HomeDocument, {
             size: 10,
             aggregations: {
-                test: getPercentileAggregation('propertyAreaSquared', [25, 50, 75])
+                testAggregation: getPercentileAggregation('propertyAreaSquared', [25, 50, 75])
             }
         })
 
-        expect(result.aggregations.test).toStrictEqual({
+        expect(result.aggregations.testAggregation).toStrictEqual({
             values: {
                 '25.0': expect.any(Number),
                 '50.0': expect.any(Number),
@@ -43,5 +44,41 @@ describe('getPercentileAggregation', () => {
         })
     })
 
-    test.todo('accepts only schema field with keyword')
+    it('should return an error after passing string field', async () => {
+        const service = app.get(ElasticsearchService)
+
+        await service
+            .search(HomeDocument, {
+                size: 0,
+                aggregations: {
+                    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                    testAggregation: getPercentileAggregation('address' as any, [25, 50, 75])
+                }
+            })
+            .catch(error => {
+                expect(error).toBeInstanceOf(ResponseError)
+                expect(error.message).toContain('search_phase_execution_exception: [illegal_argument_exception]')
+                expect(error.message).toContain(
+                    'Text fields are not optimised for operations that require per-document field data like aggregations and sorting, so these operations are disabled by default.'
+                )
+            })
+    })
+
+    it(`should return an error after passing string field with 'keyword'`, async () => {
+        const service = app.get(ElasticsearchService)
+
+        await service
+            .search(HomeDocument, {
+                size: 0,
+                aggregations: {
+                    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                    testAggregation: getPercentileAggregation('address.keyword' as any, [25, 50, 75])
+                }
+            })
+            .catch(error => {
+                expect(error).toBeInstanceOf(ResponseError)
+                expect(error.message).toContain('search_phase_execution_exception: [illegal_argument_exception]')
+                expect(error.message).toContain('Field [address.keyword] of type [keyword] is not supported for aggregation [percentiles]')
+            })
+    })
 })
