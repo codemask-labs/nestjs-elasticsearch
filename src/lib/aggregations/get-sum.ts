06b1012462d41a74e373b59219e993d23dd44be3
Ada Krupa <adriannakrupa@codemask.com>
2024-06-04 12:47:44 +0200

    fix: update sum aggregation

--- src/lib/aggregations/get-sum.ts
+++ src/lib/aggregations/get-sum.ts
@@ -1,26 +1,26 @@
 import { is } from 'ramda'
-import { Document, Field } from 'lib/common'
+import { Document, NumericField } from 'lib/common'
 
 export type SumAggregationScript = {
     script: string
 }
 
 export type SumAggregationField<TDocument extends Document> = {
-    field: Field<TDocument>
+    field: NumericField<TDocument>
 }
-
-export type SumAggregationBody<TDocument extends Document> = SumAggregationField<TDocument> | SumAggregationScript
 
 export type SumAggregation<TDocument extends Document> = {
-    sum: SumAggregationBody<TDocument>
+    sum: SumAggregationField<TDocument> | SumAggregationScript
 }
 
-export const getSumAggregation = <TDocument extends Document>(fieldOrScript: Field<TDocument> | SumAggregationScript): SumAggregation<TDocument> => {
-    if (!is(String, fieldOrScript)) {
-        return { sum: fieldOrScript }
+export const getSumAggregation = <TDocument extends Document>(
+    fieldOrScript: NumericField<TDocument> | SumAggregationScript
+): SumAggregation<TDocument> => {
+    if (is(String, fieldOrScript) || is(Symbol, fieldOrScript) || is(Number, fieldOrScript)) {
+        return { sum: { field: fieldOrScript } }
     }
 
     return {
-        sum: { field: fieldOrScript }
+        sum: fieldOrScript
     }
 }
