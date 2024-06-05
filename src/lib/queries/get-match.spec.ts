663c5bd4edf31df86562b4a55af3597b0213f31e
Ada Krupa <adriannakrupa@codemask.com>
2024-06-05 14:30:59 +0200

    test: add unit tests for match query

--- src/lib/queries/get-match.spec.ts
+++ src/lib/queries/get-match.spec.ts
@@ -1,3 +1,73 @@
+import { ResponseError } from '@elastic/elasticsearch/lib/errors.js'
+import { HomeDocument } from 'test/module'
+import { setupNestApplication } from 'test/toolkit'
+import { TEST_ELASTICSEARCH_NODE } from 'test/constants'
+import { ElasticsearchModule } from 'module/elasticsearch.module'
+import { ElasticsearchService } from 'module/elasticsearch.service'
+import { getMatchQuery } from './get-match'
+import { getBoolQuery } from './get-bool'
+import { getMustQuery } from './get-must'
+
 describe('getMatchQuery', () => {
-    test.todo('accepts field and value of a document')
+    const { app } = setupNestApplication({
+        imports: [
+            ElasticsearchModule.register({
+                node: TEST_ELASTICSEARCH_NODE
+            })
+        ]
+    })
+
+    it('accepts field and value of a document', () => {
+        const query = getMatchQuery<HomeDocument>('address', 'avenue')
+
+        expect(query).toEqual({
+            match: {
+                address: {
+                    query: 'avenue'
+                }
+            }
+        })
+    })
+
+    it('should query elasticsearch for match query', async () => {
+        const service = app.get(ElasticsearchService)
+
+        const query = 'church'
+        const result = await service.search(HomeDocument, {
+            size: 10,
+            query: getBoolQuery(getMustQuery(getMatchQuery('address', query)))
+        })
+
+        result.documents.forEach(document => expect(document.address.toLowerCase()).toContain(query.toLowerCase()))
+    })
+
+    it(`should query elasticsearch for match query when passing string field with 'keyword'`, async () => {
+        const service = app.get(ElasticsearchService)
+
+        const query = 'church'
+        const result = await service.search(HomeDocument, {
+            size: 10,
+            query: getBoolQuery(getMustQuery(getMatchQuery('city.keyword', query)))
+        })
+
+        expect(result.total).toEqual(expect.any(Number))
+    })
+
+    it('should query elasticsearch for match query with fuzzinness', async () => {
+        const service = app.get(ElasticsearchService)
+
+        const queryWithTypo = 'dhurch'
+        const result = await service.search(HomeDocument, {
+            size: 10,
+            query: getBoolQuery(
+                getMustQuery(
+                    getMatchQuery('address', queryWithTypo, {
+                        fuzziness: '1'
+                    })
+                )
+            )
+        })
+
+        result.documents.forEach(document => expect(document.address).toBeDefined())
+    })
 })
