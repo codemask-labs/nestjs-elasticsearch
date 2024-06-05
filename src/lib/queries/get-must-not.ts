9430f46a9f14e9062af93e54c804275b19f91627
Ada Krupa <adriannakrupa@codemask.com>
2024-06-05 15:52:56 +0200

    fix: update must not query

--- src/lib/queries/get-must-not.ts
+++ src/lib/queries/get-must-not.ts
@@ -3,12 +3,14 @@ import { TermQuery } from './get-term'
 import { TermsQuery } from './get-terms'
 import { BoolQuery } from './get-bool'
 import { ExistsQuery } from './get-exists'
+import { RangeQuery } from './get-range'
 
 export type MustNotQueryBody<TDocument extends Document> =
     | TermQuery<TDocument>
     | TermsQuery<TDocument>
     | BoolQuery<TDocument>
     | ExistsQuery<TDocument>
+    | RangeQuery<TDocument>
 
 export type MustNotQuery<TDocument extends Document> = {
     must_not: MustNotQueryBody<TDocument> | Array<MustNotQueryBody<TDocument>>
