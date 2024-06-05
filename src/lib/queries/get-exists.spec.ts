34d7ee67a82ba4569610090d2737557f5e7771fc
Ada Krupa <adriannakrupa@codemask.com>
2024-06-05 11:07:12 +0200

    test: add unit tests for exists aggregation

--- src/lib/queries/get-exists.spec.ts
+++ src/lib/queries/get-exists.spec.ts
@@ -1,9 +1,13 @@
+import { ResponseError } from '@elastic/elasticsearch/lib/errors.js'
 import { HomeDocument } from 'test/module'
 import { setupNestApplication } from 'test/toolkit'
 import { TEST_ELASTICSEARCH_NODE } from 'test/constants'
 import { ElasticsearchModule } from 'module/elasticsearch.module'
 import { ElasticsearchService } from 'module/elasticsearch.service'
 import { getExistsQuery } from './get-exists'
+import { getBoolQuery } from './get-bool'
+import { getMustQuery } from './get-must'
+import { getMustNotQuery } from './get-must-not'
 
 describe('getExistsQuery', () => {
     const { app } = setupNestApplication({
@@ -24,19 +28,51 @@ describe('getExistsQuery', () => {
         })
     })
 
-    it('queries es for range aggregation with nested aggregation', async () => {
+    it('should query elasticsearch for exists aggregation', async () => {
         const service = app.get(ElasticsearchService)
         const result = await service.search(HomeDocument, {
             size: 2,
-            query: {
-                bool: {
-                    must: getExistsQuery('propertyAreaSquared')
-                }
-            }
+            query: getBoolQuery(getMustQuery(getExistsQuery('address')))
         })
 
-        result.documents.forEach(document => {
-            expect(document.propertyAreaSquared).toBeDefined()
+        result.documents.forEach(document => expect(document.address).toBeDefined())
+        expect(result.total).toEqual(expect.any(Number))
+    })
+
+    it(`should query elasticsearch for exists aggregation when passing string field with 'keyword'`, async () => {
+        const service = app.get(ElasticsearchService)
+        const result = await service.search(HomeDocument, {
+            size: 2,
+            query: getBoolQuery(getMustQuery(getExistsQuery('address.keyword')))
         })
+
+        result.documents.forEach(document => expect(document.address).toBeDefined())
+        expect(result.total).toEqual(expect.any(Number))
+    })
+
+    it('should query elasticsearch for exists aggregation for documents that are missing an indexed values', async () => {
+        const service = app.get(ElasticsearchService)
+        const result = await service.search(HomeDocument, {
+            size: 2,
+            query: getBoolQuery(getMustNotQuery(getExistsQuery('propertyAreaSquared')))
+        })
+
+        result.documents.forEach(document => expect(document.propertyAreaSquared).toBeDefined())
+        expect(result.total).toEqual(expect.any(Number))
+    })
+
+    it('should return an error when passing empty string', async () => {
+        const service = app.get(ElasticsearchService)
+
+        await service
+            .search(HomeDocument, {
+                size: 2,
+                query: getBoolQuery(getMustQuery(getExistsQuery('' as any)))
+            })
+            .catch(error => {
+                expect(error).toBeInstanceOf(ResponseError)
+                expect(error.message).toContain('x_content_parse_exception: [x_content_parse_exception]')
+                expect(error.message).toContain('[bool] failed to parse field [must]')
+            })
     })
 })
