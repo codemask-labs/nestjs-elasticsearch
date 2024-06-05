0985e0e77931d8e1301777b8cacc21cf949c05d4
Ada Krupa <adriannakrupa@codemask.com>
2024-06-05 14:26:42 +0200

    fix: update match query

--- src/lib/queries/get-match.ts
+++ src/lib/queries/get-match.ts
@@ -1,8 +1,20 @@
 import { Document, Field, FieldType } from 'lib/common'
 
 export type MatchQueryOptions = {
+    /**
+     * @description Increases the importance of that query clause relative to others, affecting the relevance scoring of documents.
+     */
     boost?: number
+
+    /**
+     * @description Number of beginning characters left unchanged for fuzzy matching. Defaults to 0.
+     */
     prefix_length?: number
+
+    /**
+     * @description Enables approximate matching for string queries, useful for handling typos or variations in spelling.
+     */
+    fuzziness?: string
 }
 
 export type MatchQueryBody<TDocument extends Document, TKeyword extends Field<TDocument> = Field<TDocument>> = {
@@ -15,7 +27,7 @@ export type MatchQuery<TDocument extends Document, TKeyword extends Field<TDocum
 
 export const getMatchQuery = <TDocument extends Document, TKeyword extends Field<TDocument> = Field<TDocument>>(
     field: TKeyword,
-    query?: FieldType<TDocument, TKeyword>,
+    query: FieldType<TDocument, TKeyword>,
     options?: MatchQueryOptions
 ): MatchQuery<TDocument, TKeyword> => ({
     match: { [field]: { query, ...options } } as MatchQueryBody<TDocument, TKeyword>
