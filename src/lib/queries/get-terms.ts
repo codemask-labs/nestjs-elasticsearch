c038f3ddd5a82e43e0aa3417aef6509b4ed9c2fc
Ada Krupa <adriannakrupa@codemask.com>
2024-06-06 11:12:42 +0200

    fix: update terms query

--- src/lib/queries/get-terms.ts
+++ src/lib/queries/get-terms.ts
@@ -1,11 +1,11 @@
 import { Document, Field, FieldType } from 'lib/common'
-import { Order } from 'lib/enums'
 
 export type TermsQueryOptions = {
-    size?: number
-    order?: {
-        [x: string]: Order
-    }
+    /**
+     * @description Floating point number used to decrease or increase the importance of that query clause relative to others, affecting the relevance scoring of documents.
+     * The default value is 1.0.
+     */
+    boost?: number
 }
 
 export type TermsQueryBody<TDocument extends Document, TKeyword extends Field<TDocument> = Field<TDocument>> = {
@@ -18,7 +18,7 @@ export type TermsQuery<TDocument extends Document, TKeyword extends Field<TDocum
 
 export const getTermsQuery = <TDocument extends Document, TField extends Field<TDocument> = Field<TDocument>>(
     field: TField,
-    values?: Array<FieldType<TDocument, TField>>,
+    values: Array<FieldType<TDocument, TField>>,
     options?: TermsQueryOptions
 ): TermsQuery<TDocument, TField> => ({
     terms: { [field]: values, ...options } as TermsQueryBody<TDocument, TField>
