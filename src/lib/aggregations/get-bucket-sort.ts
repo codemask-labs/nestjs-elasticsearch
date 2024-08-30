55e7e81f79952a2e9e79319125cc468b16516c92
adrianboratyn <adrian.boratyn@icloud.com>
2024-08-30 14:11:10 +0200

    chore: resolve review comments

--- src/lib/aggregations/get-bucket-sort.ts
+++ src/lib/aggregations/get-bucket-sort.ts
@@ -8,15 +8,17 @@ export type BucketSort = {
     [bucketPath: string]: { order: Order }
 }
 
-// note: all field are optional, because
-// you can use bucket sort for both sorting and truncating result buckets
-// but you also can use it for only sorting or only truncating
+/**
+ * @description all field are optional, because
+ * you can use bucket sort for both sorting and truncating result buckets,
+ * but you also can use it for only sorting or only truncating
+*/
 export type BucketSortAggregationBody = {
     sort?: Array<BucketSort>
     from?: number
     size?: number
 }
 
-export const getBucketSortAggregation = (paramters: BucketSortAggregationBody): BucketSortAggregation => ({
-    bucket_sort: paramters
+export const getBucketSortAggregation = (parameters: BucketSortAggregationBody): BucketSortAggregation => ({
+    bucket_sort: parameters
 })
